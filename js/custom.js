8a9b7be149d2509886255534b92b090fa9958919
Junily <ja.marchant@duocuc.cl>
2022-11-07 00:07:05 -0300

    update custom style, js, and payment html

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,12 @@ function getYear() {
 }
 
 getYear();
+
+function hide() {
+    var x = document.getElementById("hide");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }else { 
+        x.style.display = "none";
+    }
+}
